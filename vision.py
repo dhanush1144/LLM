1a3d5a44b02a170c153ebe2ac44e946fca547c81
dhanush1144 <111751866+dhanush1144@users.noreply.github.com>
2024-07-18 08:29:05 +0530

    Update vision.py

--- vision.py
+++ vision.py
@@ -12,17 +12,17 @@ api_key = "AIzaSyBepBOJ-St1LnQ2Gws8c869uNrKgJxC8tI"  # Replace with your actual 
 genai.configure(api_key=api_key)
 model = genai.GenerativeModel('gemini-pro-vision')
 
-def get_gemini_response(input, image):
-    if input!="":
+def get_gemini_response("based on the provided image analyze the student's understanding level of the concept and provide two tables. the first table should display the topics in which the student is weak and needs improvement. the second table should display a 2 week study plan for the next test based on performance", image):
+    if input!=:
         response = model.generate_content([input, image])
     else:
         response = model.generate_content(image)
     return response.text
     
 
-st.set_page_config(page_title="Gemini Image Demo")
-st.header("Gemini Application")
-input = st.text_input("Input Prompt: ", key="input")
+st.set_page_config(page_title="Performance Analysis")
+st.header("Panimalar Analysis platform")
+# input = st.text_input("Input Prompt: ", key="input")
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
@@ -34,7 +34,7 @@ if uploaded_file is not None:
     # image.save(image_bytes, format=image.format)
     # image_bytes = image_bytes.getvalue()
 
-submit = st.button("Tell me about the image")
+submit = st.button("Provide Study plan")
 
 if submit :
     response = get_gemini_response(input, image)
