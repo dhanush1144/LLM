55517820bc1eb0c715def15fb46d3146f009c758
dhanush1144 <111751866+dhanush1144@users.noreply.github.com>
2025-05-26 00:32:48 +0530

    Update vision.py

--- vision.py
+++ vision.py
@@ -11,7 +11,7 @@ import google.generativeai as genai
 api_key = "AIzaSyBepBOJ-St1LnQ2Gws8c869uNrKgJxC8tI"  # Replace with your actual API key
 genai.configure(api_key=api_key)
 model = genai.GenerativeModel('gemini-2.0-flash')
-input="Extract all details from each of these this bills such as bill number ,date, sender address, receiver address, items, price, quantity, and all other details and display them as a table and put it into a downloadable csv file also provide the link to download that file"
+input="Extract all details from each of these this bills such as bill number ,date, sender address, receiver address, items, price, quantity, and all other details and display them as a table and put it into a downloadable excel file also provide the link to download that file"
 
 def get_gemini_response(input, image):
     if input!= "":
@@ -21,8 +21,8 @@ def get_gemini_response(input, image):
     return response.text
     
 
-st.set_page_config(page_title="Performance Analysis")
-st.header("Bill Analyzer")
+st.set_page_config(page_title="Bill Analysis")
+st.header("Bill Analyser")
 # input = st.text_input("Input Prompt: ", key="input")
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
@@ -31,9 +31,9 @@ if uploaded_file is not None:
     st.image(image, caption="Uploaded Image.", use_column_width=True)
     
 
-submit = st.button("Provide Study plan")
+submit = st.button("Analyse Bill")
 
 if submit :
     response = get_gemini_response(input, image)
-    st.subheader("The Response is")
+    st.subheader("Extracted Content")
     st.write(response)
